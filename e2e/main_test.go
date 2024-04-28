03f611b44f7c83e803ae8913f68e563880a8b4c8
Zhiwei Liang <zliang@akamai.com>
2024-04-27 20:24:14 -0400

    Comment cleanup

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -1,26 +1,6 @@
-/*
-Copyright 2021 The Kubernetes Authors.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package e2e
 
 import (
-	// If testing with a cloud vendor managed cluster uncomment one of the below dependencies to properly get authorised.
-	//_ "k8s.io/client-go/plugin/pkg/client/auth/azure" // auth for AKS clusters
-	//_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"   // auth for GKE clusters
-	//_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"  // auth for OIDC
 	"context"
 	"fmt"
 	"os"
@@ -35,9 +15,9 @@ import (
 )
 
 const (
-	TagEnvVar   = "DECORATOR_TEST_TAG"
-	RepoEnvVar  = "DECORATOR_TEST_REPO"
-	NodeNameVar = "NODE_NAME"
+	TagEnvVar  = "DECORATOR_TEST_TAG"
+	RepoEnvVar = "DECORATOR_TEST_REPO"
+
 	LabelPrefix = "decorator.linode.com"
 )
 
